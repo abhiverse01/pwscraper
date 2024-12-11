6957f4f301f46f7a0a7412f54386e7832162abd1
Abhishek Shah <www.abhishekshah007@gmail.com>
2024-12-11 11:27:22 +0545

    utilities added

--- pwscraper/utils.py
+++ pwscraper/utils.py
@@ -1,1 +1,16 @@
 # utils.py : Utility functions (utils.py) to handle file I/O and logging.
+# pwscraper/utils.py
+
+import logging
+import os
+
+def setup_logging():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    logging.basicConfig(
+        filename="logs/activity.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    logging.getLogger().addHandler(logging.StreamHandler())
