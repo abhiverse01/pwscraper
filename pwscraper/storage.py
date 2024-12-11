df796fe327f8880f37e226e7176c64e99d6c635e
Abhishek Shah <www.abhishekshah007@gmail.com>
2024-12-11 11:26:07 +0545

    storage: pwscraper storage data

--- pwscraper/storage.py
+++ pwscraper/storage.py
@@ -1,1 +1,16 @@
 # storage.py : Storage module (storage.py) to save the scraped data.
+# pwscraper/storage.py
+
+import pandas as pd
+import os
+
+def save_dataset(data, output_file):
+    if not os.path.exists(os.path.dirname(output_file)):
+        os.makedirs(os.path.dirname(output_file))
+
+    if not isinstance(data, list) or not all(isinstance(item, dict) for item in data):
+        raise ValueError("Invalid data format. Expected a list of dictionaries.")
+
+    df = pd.DataFrame(data)
+    df.to_csv(output_file, index=False)
+    print(f"Dataset saved to {output_file}")
